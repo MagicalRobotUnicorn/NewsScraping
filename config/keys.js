823b8767af0b434e21fec5817c5b324879619623
John Henry Weber <johnhenryweber@gmail.com>
2019-11-19 11:06:02 -0800

    Continuing to work on key issue

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 console.log("This is loaded");
-require("dotenv").config;
+const keys = require("dotenv").config;
+
+console.log(process.env.NYTIMES_KEY);
 
 module.exports = {
     key: process.env.NYTIMES_KEY
